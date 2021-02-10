1f02e62395219e0222088851efb3cfc96c428c36
moshe <moshekimhi91@gmail.com>
2021-02-10 18:00:11 +0200

    classify with Pointnet, working with both datasets, sampled or not

---
+++ main.py
@@ -0,0 +1,212 @@
+import numpy as np
+import math
+import random
+import os
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import itertools
+import matplotlib.pyplot as plt
+from path import Path
+from models import *
+import yaml
+import time
+#from utils import *
+
+
+
+def get_path(classes=40,sampled=True):
+    #path=os.path.join('data','ModelNet40-1024')
+
+    additianl = "-1024" if sampled else ""
+    if classes == 40:
+        return Path("data/ModelNet40"+additianl)
+    return Path("data/ModelNet10"+additianl)
+
+
+def default_transforms():
+    return transforms.Compose([
+                                PointSampler(1024),
+                                Normalize(),
+                                ToTensor()
+                              ])
+
+
+class PointSampler(object):
+    def __init__(self, output_size):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+
+    def triangle_area(self, pt1, pt2, pt3):
+        side_a = np.linalg.norm(pt1 - pt2)
+        side_b = np.linalg.norm(pt2 - pt3)
+        side_c = np.linalg.norm(pt3 - pt1)
+        s = 0.5 * (side_a + side_b + side_c)
+        return max(s * (s - side_a) * (s - side_b) * (s - side_c), 0) ** 0.5
+
+    def sample_point(self, pt1, pt2, pt3):
+        # barycentric coordinates on a triangle
+        # https://mathworld.wolfram.com/BarycentricCoordinates.html
+        s, t = sorted([random.random(), random.random()])
+        f = lambda i: s * pt1[i] + (t - s) * pt2[i] + (1 - t) * pt3[i]
+        return (f(0), f(1), f(2))
+
+    def __call__(self, mesh):
+        verts, faces = mesh
+        verts = np.array(verts)
+        areas = np.zeros((len(faces)))
+
+        for i in range(len(areas)):
+            areas[i] = (self.triangle_area(verts[faces[i][0]],
+                                           verts[faces[i][1]],
+                                           verts[faces[i][2]]))
+
+        sampled_faces = (random.choices(faces,
+                                        weights=areas,
+                                        cum_weights=None,
+                                        k=self.output_size))
+
+        sampled_points = np.zeros((self.output_size, 3))
+
+        for i in range(len(sampled_faces)):
+            sampled_points[i] = (self.sample_point(verts[sampled_faces[i][0]],
+                                                   verts[sampled_faces[i][1]],
+                                                   verts[sampled_faces[i][2]]))
+
+        return sampled_points
+
+
+class Normalize(object):
+    def __call__(self, pointcloud):
+        assert len(pointcloud.shape) == 2
+
+        norm_pointcloud = pointcloud - np.mean(pointcloud, axis=0)
+        norm_pointcloud /= np.max(np.linalg.norm(norm_pointcloud, axis=1))
+
+        return norm_pointcloud
+
+
+class RandRotation_z(object):
+    def __call__(self, pointcloud):
+        assert len(pointcloud.shape) == 2
+
+        theta = random.random() * 2. * math.pi
+        rot_matrix = np.array([[math.cos(theta), -math.sin(theta), 0],
+                               [math.sin(theta), math.cos(theta), 0],
+                               [0, 0, 1]])
+
+        rot_pointcloud = rot_matrix.dot(pointcloud.T).T
+        return rot_pointcloud
+
+
+class RandomNoise(object):
+    def __call__(self, pointcloud):
+        assert len(pointcloud.shape) == 2
+
+        noise = np.random.normal(0, 0.02, (pointcloud.shape))
+
+        noisy_pointcloud = pointcloud + noise
+        return noisy_pointcloud
+class ToTensor(object):
+    def __call__(self, pointcloud):
+        assert len(pointcloud.shape)==2
+
+        return torch.from_numpy(pointcloud)
+class PointCloudData(Dataset):
+    def __init__(self, root_dir, valid=False, folder="train", transform=default_transforms()):
+        self.root_dir = root_dir
+        folders = [dir for dir in sorted(os.listdir(root_dir)) if os.path.isdir(root_dir/dir)]
+        self.classes = {folder: i for i, folder in enumerate(folders)}
+        self.transforms = transform if not valid else default_transforms()
+        self.valid = valid
+        self.files = []
+        for category in self.classes.keys():
+            new_dir = root_dir/Path(category)/folder
+            for file in os.listdir(new_dir):
+                if file.endswith('.off'):
+                    sample = {}
+                    sample['pcd_path'] = new_dir/file
+                    sample['category'] = category
+                    self.files.append(sample)
+
+    def __len__(self):
+        return len(self.files)
+
+    def __preproc__(self, file):
+        verts, faces = read_off(file)
+        if self.transforms:
+            pointcloud = self.transforms((verts, faces))
+        return pointcloud
+
+    def __getitem__(self, idx):
+        pcd_path = self.files[idx]['pcd_path']
+        category = self.files[idx]['category']
+        with open(pcd_path, 'r') as f:
+            pointcloud = self.__preproc__(f)
+        return {'pointcloud': pointcloud,
+                'category': self.classes[category]}
+
+def read_off(file):
+    if 'OFF' != file.readline().strip():
+        raise ('Not a valid OFF header')
+    n_verts, n_faces, __ = tuple([int(s) for s in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return verts, faces
+
+
+
+
+if __name__ == "__main__":
+    #parameters and initilizations
+    random.seed = 42
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    with open(os.path.join(os.getcwd(), 'config/config.yml'), 'r') as yml_file:
+        config = yaml.load(yml_file)
+        print('------------ config ------------')
+        print(yaml.dump(config))
+        NUM_CLASSES  = config['data']['num_classes']
+        sampled_data = config['data']['sampled_data']
+        TRAIN_BATCH  = config['model']['train_batch']
+        TEST_BATCH   = config['model']['test_batch']
+        EPOCHS       = config['model']['EPOCHS']
+        SAMPLING_POINTS =config['data']['sampling_points']
+
+    path = get_path(classes=NUM_CLASSES,sampled=sampled_data)
+    folders = [dir for dir in sorted(os.listdir(path)) if os.path.isdir(path / dir)]
+    classes = {folder: i for i, folder in enumerate(folders)}
+    print(f"classes: {classes}")
+    if sampled_data:
+        import utils
+        train_transforms = transforms.Compose([
+            Normalize(),
+            RandomNoise(),
+        ])
+        train_ds = utils.ModelNetDataset(path,train=True,transform=train_transforms)
+        valid_ds = utils.ModelNetDataset(path, train=False, transform=train_transforms)
+    else:
+        train_transforms = transforms.Compose([
+            PointSampler(SAMPLING_POINTS),
+            Normalize(),
+            RandRotation_z(),
+            RandomNoise(),
+            ToTensor()
+        ])
+        train_ds = PointCloudData(path, transform=train_transforms)
+        valid_ds = PointCloudData(path, valid=True, folder='test')
+
+    train_loader = DataLoader(dataset=train_ds, batch_size=TRAIN_BATCH, shuffle=True)
+    valid_loader = DataLoader(dataset=valid_ds, batch_size=TEST_BATCH)
+
+
+    pointnet = PointNet(train_loader, valid_loader,classes=NUM_CLASSES,sampled_data=sampled_data).to(device)
+    print("---TRAINING---")
+    pointnet.train_all(epochs=EPOCHS, with_val=True)
+    """load best model from a file"""
+    #pointnet.load_state_dict(torch.load('best_PointNet_model.pth'))
+    """load best model from the atribute best_model (use when we train)"""
+    pointnet.load_state_dict(pointnet.best_model)
+    print("---TESTING---")
+    val = pointnet.test_all()
+    print('Test accuracy: %d %%' % val)
+
+
---
+++ models.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Tnet(nn.Module):
+    def __init__(self, k=3):
+        super().__init__()
+        self.k = k
+        self.conv1 = nn.Conv1d(k, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k * k)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+    def forward(self, input):
+        # input.shape == (bs,n,3)
+        bs = input.size(0)
+        xb = F.relu(self.bn1(self.conv1(input)))
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = F.relu(self.bn3(self.conv3(xb)))
+        pool = nn.MaxPool1d(xb.size(-1))(xb)
+        flat = nn.Flatten(1)(pool)
+        xb = F.relu(self.bn4(self.fc1(flat)))
+        xb = F.relu(self.bn5(self.fc2(xb)))
+
+        # initialize as identity
+        init = torch.eye(self.k, requires_grad=True).repeat(bs, 1, 1)
+        if xb.is_cuda:
+            init = init.cuda()
+        matrix = self.fc3(xb).view(-1, self.k, self.k) + init
+        return matrix
+
+class Transform(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_transform = Tnet(k=3)
+        self.feature_transform = Tnet(k=64)
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+
+    def forward(self, input):
+        matrix3x3 = self.input_transform(input)
+        # batch matrix multiplication
+        xb = torch.bmm(torch.transpose(input, 1, 2), matrix3x3).transpose(1, 2)
+
+        xb = F.relu(self.bn1(self.conv1(xb)))
+
+        matrix64x64 = self.feature_transform(xb)
+        xb = torch.bmm(torch.transpose(xb, 1, 2), matrix64x64).transpose(1, 2)
+
+        xb = F.relu(self.bn2(self.conv2(xb)))
+        xb = self.bn3(self.conv3(xb))
+        xb = nn.MaxPool1d(xb.size(-1))(xb)
+        output = nn.Flatten(1)(xb)
+        return output, matrix3x3, matrix64x64
+
+class PointNet(nn.Module):
+    def __init__(self, train_loader, val_loader, classes=40,sampled_data=False):
+        super(PointNet, self).__init__()
+        self.model_name = 'PointNet'
+        self.train_loader = train_loader
+        self.valid_loader = val_loader
+        self.best_model = None
+        self.sampled_data=sampled_data #if we use a database already sampled points
+
+        self.transform = Transform()
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, classes)
+
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.dropout = nn.Dropout(p=0.3)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input):
+        xb, matrix3x3, matrix64x64 = self.transform(input)
+        xb = F.relu(self.bn1(self.fc1(xb)))
+        xb = F.relu(self.bn2(self.dropout(self.fc2(xb))))
+        output = self.fc3(xb)
+        return self.logsoftmax(output), matrix3x3, matrix64x64
+
+    def pointnetloss(self, predictions, labels, m3x3, m64x64, alpha=0.0001):
+        criterion = torch.nn.NLLLoss()
+        bs = predictions.size(0)
+        id3x3 = torch.eye(3, requires_grad=True).repeat(bs, 1, 1)
+        id64x64 = torch.eye(64, requires_grad=True).repeat(bs, 1, 1)
+        if predictions.is_cuda:
+            id3x3 = id3x3.cuda()
+            id64x64 = id64x64.cuda()
+        diff3x3 = id3x3 - torch.bmm(m3x3, m3x3.transpose(1, 2))
+        diff64x64 = id64x64 - torch.bmm(m64x64, m64x64.transpose(1, 2))
+        return criterion(predictions, labels) + alpha * (torch.norm(diff3x3) + torch.norm(diff64x64)) / float(bs)
+
+    def train_all(self, epochs=10, with_val=True):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        best_acc = -1
+        for epoch in range(epochs):
+            self.train()
+            running_loss = 0.0
+            total = correct = 0
+            for i, data in enumerate(self.train_loader, 0):
+                if not self.sampled_data:
+                    inputs, labels = data['pointcloud'].to(device).float(), data['category'].to(device)
+                    optimizer.zero_grad()
+                    outputs, m3x3, m64x64 = self(inputs.transpose(1, 2))  # forward
+                else:
+                    inputs, labels = data#['pointcloud'].to(device).float(), data['category'].to(device)
+                    optimizer.zero_grad()
+                    outputs, m3x3, m64x64 = self(inputs)  # forward
+
+
+                predicted = torch.argmax(outputs,1)
+
+                total += labels.size(0)
+                correct += (labels == predicted).sum().item()
+                loss = self.pointnetloss(outputs, labels, m3x3, m64x64)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 10 == 9:
+                    print(f'Epoch: {epoch + 1}, Batch: {i + 1}, loss: {running_loss / 10} Train accuracy: {correct/total}')
+                    running_loss = 0.0
+                    total = correct = 0
+
+            # validation
+            if with_val:
+                val_acc = self.test_all()
+                if val_acc > best_acc:
+                    best_acc = val_acc
+                    self.best_model = copy.deepcopy(self.state_dict())
+                print('Valid accuracy: %d %%' % val_acc)
+            torch.save(self.best_model, f"best_{self.model_name}_model.pth")
+
+    def test_all(self):
+        self.eval()
+        correct = total = 0
+        with torch.no_grad():
+            for data in self.valid_loader:
+                if not self.sampled_data:
+                    inputs, labels = data['pointcloud'].to(device).float(), data['category'].to(device)
+                    outputs, _, _ = self(inputs.transpose(1, 2))  # forward
+                    _, predicted = torch.max(outputs.data, 1)
+                else:
+                    inputs, labels = data
+                    outputs, _, _ = self(inputs)  # forward
+                    predicted = torch.argmax(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        val_acc = 100. * correct / total
+        return val_acc
+
